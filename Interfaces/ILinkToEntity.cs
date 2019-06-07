70511234c01cbaafe7743761441f6f03c63afb7e
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-07 14:48:06 -0700

    Adds ILinkToEntity

---
+++ Interfaces/ILinkToEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Builders.Interfaces
+{
+    /// <summary>
+    /// Compile-time marker interface, no need for methods
+    /// This lets the services know which entity class to map the DTO to
+    /// </summary>
+    public interface ILinkToEntity<TEntity> : ILinkToEntity where TEntity : IEntity
+    {
+
+    }
+
+    //TODO: Make this generic just like IEntity
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ILinkToEntity : IModel
+    {
+        int Id { get; set; }
+    }
+}
