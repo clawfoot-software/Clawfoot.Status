91206f95178f03869e4fa0eeff152a5eab68e19e
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-07 14:02:01 -0700

    Adds comments to IModelDefaults

--- Interfaces/IModelsDefaults.cs
+++ Interfaces/IModelsDefaults.cs
@@ -6,7 +6,18 @@ namespace Clawfoot.Builders.Interfaces
 {
     public interface IModelsDefaults
     {
+        /// <summary>
+        /// Adds the default values for a model to the cache
+        /// </summary>
+        /// <typeparam name="T">The type of the model</typeparam>
+        /// <param name="defaultValues"></param>
         void Add<T>(IModelDefaultValues<T> defaultValues) where T : new();
+
+        /// <summary>
+        /// Gets the defaults for the model
+        /// </summary>
+        /// <typeparam name="T">The type of the model</typeparam>
+        /// <returns></returns>
         IModelDefaultValues<T> Get<T>() where T : new();
     }
 }
