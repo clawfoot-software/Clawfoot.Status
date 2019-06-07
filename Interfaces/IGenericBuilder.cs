ec6699d88eeb89a774f5e6f506eccce762698cc5
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-07 12:34:39 -0700

    Adds IGenericBuilder adds IModelDefaultValues

---
+++ Interfaces/IGenericBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Builders.Interfaces
+{
+    /// <summary>
+    /// A generic builder used to build any model type
+    /// </summary>
+    /// <typeparam name="TModel">The model type</typeparam>
+    public interface IGenericBuilder<TModel> : IBuilder<TModel> where TModel : new()
+    {
+        /// <summary>
+        /// Add an action that sets the appropriate model property. Is chainable.
+        /// </summary>
+        /// <example>
+        ///     <code>With(x => x.Name = "Name")</code>
+        /// </example>         
+        /// <param name="with">The action to set the property</param>
+        IGenericBuilder<TModel> With(Action<TModel> with);
+
+        /// <summary>
+        /// Applies the preconfigured defaults, or the provided defaults to the model
+        /// </summary>
+        /// <param name="defaults">Optional default values for the model</param>
+        IGenericBuilder<TModel> UseDefaults(IModelDefaultValues<TModel> defaults = null);
+    }
+}
---
+++ Interfaces/IModelDefaultValues.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Builders.Interfaces
+{
+    /// <summary>
+    /// Marker interface used for upcasting from a generic method
+    /// </summary>
+    public interface IModelDefaultValues
+    {
+
+    }
+
+    /// <summary>
+    /// A set of default values for a Model type.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IModelDefaultValues<TModel> : IModelDefaultValues
+    {
+        /// <summary>
+        /// The array of default value setting actions
+        /// </summary>
+        IList<Action<TModel>> Actions { get; }
+
+        /// <summary>
+        /// Adds a default value setter. Can be chained.
+        /// </summary>
+        /// <example>
+        ///     <code>Add(x => x.Name = "Name")</code>
+        /// </example> 
+        IModelDefaultValues<TModel> Add(Action<TModel> action);
+
+        /// <summary>
+        /// Adds an array of default value setters
+        /// </summary>
+        /// <param name="actions"></param>
+        IModelDefaultValues<TModel> Add(params Action<TModel>[] actions);
+    }
+}
