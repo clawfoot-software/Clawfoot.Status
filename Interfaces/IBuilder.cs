2cea637c0c447bbd01db2223f7ef93e7afd0f9cc
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-07 10:34:44 -0700

    Adds IBuilder interface

---
+++ Interfaces/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Builders.Interfaces
+{
+    public interface IBuilder<T> where T : new()
+    {
+        T Build(bool fillInForeignKeysIds = true);
+        T Build(T built, bool fillInForeignKeysIds = true);
+    }
+}
