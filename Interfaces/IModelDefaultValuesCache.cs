d69008b489b43c6157446e33fe93eb331e20e922
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-07 14:06:30 -0700

    Renames IModelsDefaults to IModelDefaulValuesCache
    More semantic naming

--- Interfaces/IModelDefaultValuesCache.cs
+++ Interfaces/IModelDefaultValuesCache.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Clawfoot.Builders.Interfaces
 {
-    public interface IModelsDefaults
+    public interface IModelDefaultValuesCache
     {
         /// <summary>
         /// Adds the default values for a model to the cache
@@ -14,10 +14,10 @@ namespace Clawfoot.Builders.Interfaces
         void Add<T>(IModelDefaultValues<T> defaultValues) where T : new();
 
         /// <summary>
-        /// Gets the defaults for the model
+        /// Gets the defaults for the model from the cache
         /// </summary>
         /// <typeparam name="T">The type of the model</typeparam>
-        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         IModelDefaultValues<T> Get<T>() where T : new();
     }
 }
