cf780cd7516308056ff3dfed9b824fd1905397eb
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-06-10 17:52:58 -0700

    Adds builder class with empty cosntructor
    This enables use without a DI container

---
+++ Builder.cs
@@ -0,0 +1,18 @@
+﻿using Clawfoot.Builders.Interfaces;
+using Clawfoot.Utilities.Caches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Builders
+{
+    public class Builder<TModel> : BuilderBase<TModel>, IGenericBuilder<TModel> where TModel : new()
+    {
+        public Builder()
+            :this(new ModelDefaultValuesCache(), new ForeignKeyPropertyCache()){ }
+
+        public Builder(IModelDefaultValuesCache modelDefaults, IForeignKeyPropertyCache propertyCache)
+            :base(modelDefaults, propertyCache)
+        { }
+    }
+}
