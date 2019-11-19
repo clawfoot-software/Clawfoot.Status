f9adc7da79745721366bcd4b58893d35be92afa1
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-11-18 16:25:08 -0800

    Adds Status MapTo() extension method to Extensions.Automapper

---
+++ Clawfoot.Extensions.Automapper/StatusExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Clawfoot.Status.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clawfoot.Extensions.Automapper
+{
+    public static class StatusExtensions
+    {
+        public static IStatus<TToResult> MapResultTo<TFromResult, TToResult>(this IStatus<TFromResult> status, IMapper mapper)
+        {
+            TToResult result = mapper.Map<TToResult>(status.Result);
+            return status.ConvertTo<TToResult>(result);
+        }
+    }
+}
